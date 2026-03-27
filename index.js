05c59eeff862f82ff9e67466f5c7cb6fbf01663e
OmiKid75 <naomiumburi@gmail.com>
2026-03-27 13:43:23 +0300

    fix: fix index.js setup, add methodOverride for delete, connect DB and routes

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import studentApiRoutes from "./routes/student.routes.js";
+import studentViewRoutes from "./routes/student.view.routes.js";
+import methodOverride from "method-override";
+
+
+dotenv.config();
 
 const app = express();
 
+// Connect to MongoDB
+connectDB();
+
+app.use(methodOverride("_method"));
+// View engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/students", studentApiRoutes);
+app.use("/view", studentViewRoutes);
+
+// Redirect root to students page
 app.get("/", (req, res) => {
   res.redirect("/view/students");
 });
@@ -9,5 +36,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
